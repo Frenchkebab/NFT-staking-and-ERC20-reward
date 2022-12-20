50c5bfd4ffc3030112abe0eafac896069e1ff51d
frenchkebab <tubowangin@gmail.com>
2022-12-20 18:54:24 +0900

    Upgradeable NFT Staking contract

--- contracts/NFTStaker.sol
+++ contracts/NFTStaker.sol
@@ -1,55 +1,63 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract NFTStaker is IERC721Receiver {
-    IERC721 public myNFT;
-    IERC20 public myERC20;
+/**
+ * @title Upgradeable NFT staking contract
+ * @author Junghyun Choi (@Frenchkebab)
+ * @notice
+ */
 
-    // Need to stake at least 10 seconds to get reward
-    uint256 constant REWARD_TIME = 10;
+contract NFTStaker is Initializable, OwnableUpgradeable, IERC721ReceiverUpgradeable, UUPSUpgradeable {
+    IERC721 public nftContract;
+    IERC20 public erc20Contract;
+
+    /// @dev Need to stake at least 24 hours to get reward
+    uint256 constant REWARD_TIME = 24 hours;
 
     struct StakedToken {
         address tokenOwner;
         uint256 timestamp;
     }
 
-    // map tokenId to stake details
+    /// @dev map tokenId to stake details
     mapping(uint256 => StakedToken) public stakes;
 
-    // map tokenId to total staking time
-    mapping(uint256 => uint256) public stakingTime;
-
-    constructor(IERC721 _NFTContractAddress, IERC20 _tokenContractAddress) {
-        myNFT = IERC721(_NFTContractAddress);
-        myERC20 = IERC20(_tokenContractAddress);
+    function initialize(IERC20 _tokenContractAddress, IERC721 _NFTContractAddress) public initializer {
+        nftContract = IERC721(_NFTContractAddress);
+        erc20Contract = IERC20(_tokenContractAddress);
     }
 
     function stake(uint256 _tokenId) external {
-        require(myNFT.ownerOf(_tokenId) == msg.sender, "Caller is not owner of the token");
+        require(nftContract.ownerOf(_tokenId) == msg.sender, "NFTStaker: Caller is not owner of the token");
         stakes[_tokenId] = StakedToken(msg.sender, block.timestamp);
-        myNFT.safeTransferFrom(msg.sender, address(this), _tokenId, "0x00");
+        nftContract.safeTransferFrom(msg.sender, address(this), _tokenId, "0x00");
     }
 
-    // checks if the token has been staked more than REWARD_TIME
-    // resets after unstaking
+    /**
+     * @dev checks if the token has been staked more than REWARD_TIME
+     * resets after unstaking
+     */
+    //
     function unstake(uint256 _tokenId) external {
-        require(stakes[_tokenId].tokenOwner == msg.sender, "Caller is not owner of the token");
-        myNFT.safeTransferFrom(address(this), msg.sender, _tokenId, "0x00");
+        require(stakes[_tokenId].tokenOwner == msg.sender, "NFTStaker: Caller is not owner of the token");
+        nftContract.safeTransferFrom(address(this), msg.sender, _tokenId, "0x00");
         uint256 stakedTime = block.timestamp - stakes[_tokenId].timestamp;
 
-        if (stakedTime >= REWARD_TIME) {
-            uint256 cycles = stakedTime / REWARD_TIME;
-            uint256 reward = 10 * 10 ** 18 * cycles;
-            myERC20.transfer(msg.sender, reward);
-        }
+        require(stakedTime >= REWARD_TIME, "NFTStaker: Need to wait more to unstake");
+        uint256 cycles = stakedTime / REWARD_TIME;
+        uint256 reward = 10 * 10 ** 18 * cycles;
+        erc20Contract.transfer(msg.sender, reward);
+
         delete stakes[_tokenId];
     }
 
-    // This function checks if the receiver can understand what's going on
     function onERC721Received(
         address operator,
         address from,
@@ -58,4 +66,9 @@ contract NFTStaker is IERC721Receiver {
     ) external override returns (bytes4) {
         return this.onERC721Received.selector;
     }
+
+    /**
+     * @dev required by the OZ UUPS module
+     */
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 }
