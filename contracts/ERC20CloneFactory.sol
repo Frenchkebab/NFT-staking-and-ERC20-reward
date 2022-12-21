a8126d31757bacb422835f1e300566f36c22eb8e
frenchkebab <tubowangin@gmail.com>
2022-12-22 00:17:33 +0900

    ERC20 Clone test script

--- contracts/ERC20CloneFactory.sol
+++ contracts/ERC20CloneFactory.sol
@@ -7,18 +7,17 @@ import "./ERC20Implementation.sol";
 contract ERC20CloneFactory {
     address implementation;
 
+    event Clone(address indexed owner, address instanceAddr);
+
     constructor(address _implementation) {
         implementation = _implementation;
     }
 
-    function clone(
-        string calldata _name,
-        string calldata _symbol,
-        address _owner,
-        uint256 _cap
-    ) external returns (address) {
+    function clone(string calldata _name, string calldata _symbol, uint256 _cap) external returns (address) {
         address clone = Clones.clone(implementation);
-        ERC20Implementation(clone).initialize(_name, _symbol, _owner, _cap);
+        ERC20Implementation(clone).initialize(_name, _symbol, msg.sender, _cap);
+
+        emit Clone(msg.sender, clone);
         return clone;
     }
 }
